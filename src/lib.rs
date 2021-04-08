382937699a71aabec260d9fdf4657599dfd1315a
mansong1 <martin.ansong@gmail.com>
2021-04-08 17:44:48 +0100

    Add tera and create function to render templates

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
 pub mod configuration;
 
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder, Result};
 use std::net::TcpListener;
+use tera::{Context, Tera};
 
 async fn health_check(_req: HttpRequest) -> impl Responder {
     HttpResponse::Ok().finish()
@@ -13,9 +14,24 @@ async fn greet(req: HttpRequest) -> impl Responder {
     format!("Hello {}!", &name)
 }
 
+async fn render_tmpl(data: web::Data<AppData>, req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap();
+    let mut ctx = Context::new();
+    ctx.insert("name", name);
+    let rendered = data.tmpl.render("index.html", &ctx).unwrap();
+    HttpResponse::Ok().body(rendered)
+}
+
+struct AppData {
+    tmpl: Tera,
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
+        let tera = Tera::new(concat!(env!("CARGO_MANIFEST_DIR"), "/templates/**/*")).unwrap();
         App::new()
+            .data(AppData { tmpl: tera })
+            .service(web::resource("/tmpl/{name}").route(web::get().to(render_tmpl)))
             .route("/", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
     })
